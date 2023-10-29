8e8710f477f1b0b95985671e15a33d523baf9507
maria <maraialipour09@gmail.com>
2023-10-29 08:38:14 +0330

    CarouselSlider

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shik_poush/data/models/home.dart';
 import 'package:shik_poush/screens/home/bloc/home_bloc.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:cached_network_image/cached_network_image.dart';
@@ -44,28 +45,7 @@ class _HoomeScreenState extends State<HoomeScreen> {
               padding: EdgeInsets.fromLTRB(16, 0, 16, 50),
               child: Column(
                 children: [
-                  CarouselSlider(
-                    options: CarouselOptions(
-                        height: 160.0,
-                        viewportFraction: 1,
-                        enlargeCenterPage: true,
-                        autoPlay: true),
-                    items: state.homeEntity.slides!.map((i) {
-                      return ClipRRect(
-                        borderRadius: BorderRadiusDirectional.circular(12),
-                        child: CachedNetworkImage(
-                          imageUrl: "https://flutter.vesam24.com/${i.image}",
-                          fit: BoxFit.fill,
-                          placeholder: (context, url) => const Center(
-                            child: CupertinoActivityIndicator(),
-                          ),
-                          errorWidget: (context, url, error) => const Center(
-                            child: Icon(Icons.error),
-                          ),
-                        ),
-                      );
-                    }).toList(),
-                  ),
+                  homeSlider(state.homeEntity),
                 ],
               ),
             );
@@ -80,6 +60,64 @@ class _HoomeScreenState extends State<HoomeScreen> {
           }
         },
       ),
+    );
+  }
+}
+
+class homeSlider extends StatefulWidget {
+  final HomeEntity homeEntity;
+  const homeSlider(this.homeEntity, {super.key});
+
+  @override
+  State<homeSlider> createState() => _homeSliderState();
+}
+
+class _homeSliderState extends State<homeSlider> {
+  int currentSliderIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Stack(alignment: Alignment.center,
+      children: [
+        CarouselSlider(
+          options: CarouselOptions(
+              onPageChanged: (index, reason) {
+                setState(() {
+                  currentSliderIndex = index;
+                });
+              },
+              height: 160.0,
+              viewportFraction: 1,
+              enlargeCenterPage: true,
+              autoPlay: true),
+          items: widget.homeEntity.slides!.map((i) {
+            return ClipRRect(
+              borderRadius: BorderRadiusDirectional.circular(12),
+              child: CachedNetworkImage(
+                imageUrl: "https://flutter.vesam24.com/${i.image}",
+                fit: BoxFit.fill,
+                placeholder: (context, url) => const Center(
+                  child: CupertinoActivityIndicator(),
+                ),
+                errorWidget: (context, url, error) => const Center(
+                  child: Icon(Icons.error),
+                ),
+              ),
+            );
+          }).toList(),
+        ),
+        Positioned(
+          bottom:12 ,
+          child: AnimatedSmoothIndicator(
+            activeIndex: currentSliderIndex ,
+             count: widget.homeEntity.slides!.length,
+             effect: ExpandingDotsEffect(
+              dotHeight: 5,
+             dotWidth: 6,
+             activeDotColor: Colors.blue,
+             dotColor: Colors.grey.shade100,
+             ),),
+        ),
+      ],
     );
   }
 }
